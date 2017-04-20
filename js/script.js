1f8beabbc734d2889b42644fdc5735c701709346
Di <yaodi891024@gmail.com>
2017-04-20 17:48:20 -0500

    add the logic for component, and add css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 angular.module("app",[])
 	.controller("contactSlide", ["$scope",function($scope){
 
+		$scope.show = false;
+		$scope.form = {} ;
+
+		$scope.openForm = function(){
+			$scope.form = {};
+			$scope.show = true;
+		}
+		$scope.closeForm = function(){
+			$scope.show = false;
+		}
+		$scope.submitForm = function(){
+			var text = "Firstname is " + $scope.form.firstName + "\n" + "Lastname is " + $scope.form.lastName + "\n" + "Message is " + $scope.form.message;
+			console.log(text);
+		}
+		
 	}])
 	.directive("contactForm", function(){
 		return {
